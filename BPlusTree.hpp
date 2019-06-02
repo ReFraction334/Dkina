c63c1406a81300adf69a1c66598a8dd9c7a624e6
Yi Wang <44198739+ReFraction334@users.noreply.github.com>
2019-06-02 22:04:09 +0800

    bufferPoolsize try 5000

--- BPlusTree.hpp
+++ BPlusTree.hpp
@@ -61,7 +61,7 @@ private:
 	};
     class BufferPool
     {
-        static const int BufferPoolSizeLimit = 500;
+        static const int BufferPoolSizeLimit = 5000;
         struct BPtreeNode
         {
             Node nd;
